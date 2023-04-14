f6af4746df229faa49b9531a5418b63df5b39aca
Ivan_Pun <whitetiger123asd@gmail.com>
2023-04-14 18:46:32 +0800

    update

--- js/personal.js
+++ js/personal.js
@@ -1,29 +1,34 @@
 //Get Personal Data
 $(document).ready(function(){
     var name=localStorage.getItem("name");
-    if(id!=""){
-        const Url='https://893fb54.r15.cpolar.top/getUserByName/'+id;
+    if(name!=""){
+        const Url='https://893fb54.r15.cpolar.top/getUserByName/'+name;
         $.ajax({
             url: Url,
             type: "POST",
             success:function(result){
                 console.log(result);
+                if(result.photo==null){
+                  var photo= 'test-person-icon';
+                }else{
+                  var photo = result.photo;
+                }
                 var html="";
                 html =  '<div class="container" id="personal_data">'+
-                        '<img src="photos/test-person-icon.jpg" class="mx-auto d-block personal-icon rounded-circle" >'+
+                        '<img src="photos/'+ photo +'.jpg" class="mx-auto d-block personal-icon rounded-circle" >'+
                         '<a href="change_personal_data.html"><button type="button" class="btn btn-outline-warning mx-auto d-block" id="change_personal_data">修改个人信息</button></a>'+
                         '<div class="card" style="width: 100%;margin-top: 5%;">'+
                         '<ul class="list-group list-group-flush">'+
-                        '<li class="list-group-item">昵称：Ivan</li>'+
-                        '<li class="list-group-item">邮箱：whitetiger123asd@gmail.com</li>'+
-                        '<li class="list-group-item">Never Give Up</li>'+
+                        '<li class="list-group-item">'+result.name+'</li>'+
+                        '<li class="list-group-item">邮箱：'+ result.email +'</li>'+
+                        '<li class="list-group-item">'+result.signature+'</li>'+
                         '</ul>'+
                         '</div>'+
                         '<button type="button" class="btn btn-secondary" style="margin-top: 5%;">登出</button><br>'+
                         '<button type="button" class="btn btn-danger" style="margin-top: 5%;">注销</button><br>'+
                         '<button type="button" class="btn btn-outline-success" style="margin-top: 5%;float: right;">设为公开</button>'+
                         '</div>'
-                $("#personal_data").html(html);
+                $("#personal_data").append(html);
             }
         })
     }else{
@@ -33,7 +38,7 @@ $(document).ready(function(){
 
 //Change Personal Data
 $("#change_personal_data").click(function(){
-    var id=localStorage.getItem("id");
+  var name=localStorage.getItem("name");
     if (id!="") {
         const Url='https://893fb54.r15.cpolar.top/userLogin/'+name+'/'+password;
         $.ajax({
