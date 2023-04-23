79d573a2f9efd2dde710624032475682c979c890
Ivan_Pun <whitetiger123asd@gmail.com>
2023-04-23 23:07:50 +0800

    update

--- js/change_personal.js
+++ js/change_personal.js
@@ -17,7 +17,13 @@ $(document).ready(function(){
               html =  '<div class="card">'+
                       '  <div class="card-body">'+
                       '      <img src="'+photo+'" class="mx-auto d-block personal-icon rounded-circle" >'+
-                      '      <a href=""><button type="button" class="btn btn-outline-warning mx-auto d-block" id="change_icon">修改个人头像</button></a>'+
+                      '     <form  method="post" enctype="multipart/form-data">'+
+                      '         <div class="mb-3">'+
+                      '           <div class="mb-3">'+
+                      '             <input class="form-control" type="file" id="formFile" name="file">'+
+                      '           </div>'+
+                      '         <button type="button" class="btn btn-outline-warning mx-auto d-block" id="changeicon">修改个人头像</button>'+
+                      '     </form>'+
                       '      <div class="form-floating mb-3">'+
                       '          <input type="text" class="form-control" id="newname" placeholder="null">'+
                       '          <label for="floatingInput">更改个人昵称</label>'+
@@ -75,11 +81,12 @@ $("#change_data").click(function(){
       }
 })
 
-
+/*
 //changeicon
 $(document).ready(function() {
     $('#changeicon').submit(function(event) {
       event.preventDefault();
+      alert("why i am working");
       var formData = new FormData();
       var name=localStorage.getItem("name");
       formData.append('name',name);
@@ -102,7 +109,44 @@ $(document).ready(function() {
       });
       return false;
     });
-  });
+  });*/
+
+  
+$(document).on('click','#changeicon',function(){
+  
+    if($('#formFile')[0].files[0]==undefined){
+      document.getElementById("btn2").click();
+    }else{
+      var formData = new FormData();
+      var name=localStorage.getItem("name");
+      formData.append('name',name);
+      formData.append('photo',$('#formFile')[0].files[0]);
+
+      console.log($('#formFile')[0].files[0]);
+      $.ajax({
+        url: 'http://124.71.207.55:8081/uploadUserImgByName',
+        type: 'POST',
+        data: formData,
+        async: false,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function(response) {
+          console.log(response);
+        },
+        error: function(response) {
+          console.log(response);
+        }
+      });
+      document.getElementById("btn1").click();
+    }  
+});
+
+$("#finish").click(function(){
+  location.href=("personal.html");
+})
+
+
 
   
 
