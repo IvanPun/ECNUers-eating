dd106922adf19d9f72e6b6fdf9ce90a3935abad6
qingningv <113343810+qingningv@users.noreply.github.com>
2023-04-23 17:03:03 +0800

    Add files via upload

--- js/recordupload.js
+++ js/recordupload.js
@@ -5,16 +5,31 @@ upload.addEventListener("change", function (e) {
   // 掉axios请求接口上传文件
   var formData = new FormData();
   console.log(upload.files);
-  formData.append("name", "测试菜名");
+  var gk = document.getElementsByName("flexRadioDefault");
+  let setgk = "";
+  for (let i = 0; i < gk.length; i++) {
+    gk[i].addEventListener("change", function () {
+      if (gk[i].checked) {
+        setgk = gk[i].value;
+        console.log(gk[i].value);
+        formData.append("pub", setgk);
+      }
+    });
+  }
+  formData.append("name", localStorage.getItem("name"));
+  formData.append("majority", $(".textbz").val());
   formData.append("photo", upload.files[0]);
+
   // 发送POST请求，上传文件到服务器
-  $(".btnupload").click(function () {
+$(".userdk").click(function () {
     axios
-      .post("http://124.71.207.55:8081/uploadDishImgByName", formData)
+      .post("http://124.71.207.55:8081/addPostByName", formData)
       .then(function (response) {
+        var text=$(".textbz").val();
+        console.log(text);
         console.log("文件上传成功", response);
         if (response.status == 200) {
-          alert("上传成功");
+          alert("打卡成功");
         }
       })
       .catch(function (error) {
